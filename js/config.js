61dc290238810f1150ed7a6f0c5ed8cfee5ebf7b
ntcong3112 <74366485+ntcong3112@users.noreply.github.com>
2022-07-23 17:50:51 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+export default {
+  name: "Hùng", // actual name of the recipient (Mandatory)
+
+  nickname: "Meangung", // nickname(optional)
+
+  pic: "", // image url of recipients (Mandatory)
+
+  showScrollMsg: true, // set to false if you do not want the scrolling message
+
+  openDate: false, // the page only can be opened at this time, read README.md
+
+  timeZone: "Asia/Jakarta", // timezone to use openDate feature
+};
